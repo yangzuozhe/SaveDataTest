f2388dabb83f2e4b0f497267ec517b795d677bdf
yangzuozhe <yangzuozhe@taqu.cn>
2021-01-15 17:07:28 +0800

    安卓内存的读写

--- app/src/main/java/com/example/savedatatest/FileHelper.java
+++ app/src/main/java/com/example/savedatatest/FileHelper.java
@@ -2,9 +2,12 @@ package com.example.savedatatest;
 
 import android.content.Context;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class FileHelper {
     Context mContext;
@@ -18,28 +21,74 @@ public class FileHelper {
     }
 
     /**
+     * 通过字节流的方式存储数据
      * 这里定义的是一个文件保存的方法，写入到文件中，所以是输出流
+     *
+     * @param fileName    文件名称
+     * @param fileContent 文件内容
+     * @throws Exception
      */
-    public void save(String fileName, String fileContent) throws Exception {
-        FileOutputStream outputStream = mContext.openFileOutput(fileName, Context.MODE_APPEND);
-        //将String字符串以字节流的形式写入到输出流中
-        outputStream.write(fileContent.getBytes());
-        //关闭输出流
-        outputStream.close();
+    public void save(String fileName, String fileContent) {
+        //
+        FileOutputStream outputStream = null;
+        try {
+            //MODE_PRIVATE 是默认的操作的模式，如果文件存在，新的数据会，覆盖，文件旧的内容。
+            //MODE_APPEND 是表示如果文件存在就会往文件的，末尾，添加新的数据
+            //通过 openFileOutput 打开输出流
+            outputStream = mContext.openFileOutput(fileName, Context.MODE_APPEND);
+            //将String字符串以字节流的形式写入到输出流中
+            outputStream.write(fileContent.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //关闭输出流
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
-    public String read(String fileName) throws IOException {
+    /**
+     * 通过字节流读取本地存储的文件
+     *
+     * @param fileName 文件名称
+     * @return 文件的内容
+     * @throws IOException
+     */
+    public String read(String fileName) {
         //打开文件输入流
-        FileInputStream inputStream = mContext.openFileInput(fileName);
-        byte[] temp = new byte[1024];
+        FileInputStream inputStream = null;
+        //拼接字符串
         StringBuilder sb = new StringBuilder("");
-        int len = 0;
-        //读取文件内容
-        while ((len = inputStream.read(temp)) > 0) {
-            sb.append(new String(temp,0,len));
+        try {
+            //通过openFileInput 打开输入流
+            inputStream = mContext.openFileInput(fileName);
+            //准备一个字节数组容器
+            byte[] temp = new byte[1024];
+            int len = 0;
+            //读取文件内容
+            //将读取的流获取的数据存放到字节容器当中。
+            while ((len = inputStream.read(temp)) > 0) {
+                //通过String 的实例化将字节数组，转化为 字符串，然后拼接
+                sb.append(new String(temp, 0, len));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭输入流
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        //关闭输入流
-        inputStream.close();
         return sb.toString();
     }
 
--- app/src/main/java/com/example/savedatatest/MainActivity.java
+++ app/src/main/java/com/example/savedatatest/MainActivity.java
@@ -6,15 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText mEdName;
     EditText mEdContent;
+    EditText mEdDelete;
     Button mBtnSave;
     Button mBtnClear;
     Button mBtnLoad;
+    Button mBtnDelete;
+    String mFileName;
+    String mContent;
+
+    FileHelper helper = new FileHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnClear.setOnClickListener(this);
         mBtnLoad = findViewById(R.id.btnLoad);
         mBtnLoad.setOnClickListener(this);
+        mBtnDelete = findViewById(R.id.btnDelete);
+        mBtnDelete.setOnClickListener(this);
+        mEdDelete = findViewById(R.id.edDelete);
     }
 
 
@@ -39,11 +49,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.btnSave) {
-
+            Save();
         } else if (i == R.id.btnLoad) {
-
+            load();
         } else if (i == R.id.btnClear) {
-
+            clear();
+        } else if (i == R.id.btnDelete) {
+            delete();
         }
     }
+
+    /**
+     * data目录里 文件的存储
+     */
+    public void Save() {
+        mFileName = mEdName.getText().toString();
+        mContent = mEdContent.getText().toString();
+        if (!isEmpty(mFileName) && !isEmpty(mContent)) {
+            helper.save(mFileName, mContent);
+        }
+    }
+
+    /**
+     * data目录里 文件的读取
+     */
+    public void load() {
+        mFileName = mEdName.getText().toString();
+        if (!isEmpty(mFileName)) {
+            Toast.makeText(this, helper.read(mFileName), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * data目录保留前5个文件
+     */
+    public void clear() {
+        //获取 app的data目录下的全部文件
+        String[] list = fileList();
+        //只保留前5个文件
+        for (int j = list.length - 1; j > 4; j--) {
+            deleteFile(list[j]);
+        }
+    }
+
+    /**
+     * data目录里 文件的删除
+     */
+    public void delete() {
+        String deleteText = mEdDelete.getText().toString();
+        if (!isEmpty(deleteText)) {
+            //删除指定的文件，如果为真删除成功，如果为假，没有这个文件
+            if (deleteFile(deleteText)) {
+                Toast.makeText(MainActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "没有这个文件", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    public boolean isEmpty(String s) {
+        return s == null || s.trim().length() == 0 || "null".equals(s);
+    }
+
 }
